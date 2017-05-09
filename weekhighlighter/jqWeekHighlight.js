1e82238596a9617398142f624e4ea37d6257866a
RadioLogic <nrc1mr.e3@gmail.com>
2017-05-09 10:04:16 -0400

    changed it for teacher

--- weekhighlighter/jqWeekHighlight.js
+++ weekhighlighter/jqWeekHighlight.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-	var d=new Date()
-	function week(){
+	var d = new Date()
+	function week() {
 		var weekBase = (((d.getMonth()*31)+d.getDate()-(d.getDay()+1))/7)-1;
 		if (d.getDay() <= 6) {
 			weekBase += 1;
-		};
+		}
 		return Math.round(weekBase);
-	};
-	$("#week").append("week"+week());
-	$("#week"+week()).addClass("currWeek");
-	$("#weekday").append(d.getMonth());
-	$("#amountDay").append((d.getMonth()*31)+d.getDate());
+	}
+	var schoolWeek=week();
+	if (((d.getMonth()*31)+d.getDate())>=102) {
+		schoolWeek = schoolWeek - 1; //spring break
+	}
+	if (((d.getMonth()*31)+d.getDate())>=123) {schoolWeek=schoolWeek-1;} //fix rounding issue
+	$("#WK"+schoolWeek).addClass("currentWeek");
 });
